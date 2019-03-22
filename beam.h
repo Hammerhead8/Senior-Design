ddd24c6b8d3d98eb16d86d64e0c23d7435eb17b7
Hammerhead8 <48372774+Hammerhead8@users.noreply.github.com>
2019-03-22 14:56:16 -0400

    Update beam.h

--- beam.h
+++ beam.h
@@ -12,9 +12,12 @@ class
 Beam
 {
   public:
-    volatile uint8_t note = 0;
-    volatile uint16_t freq = -1;
-    volatile uint16_t freqReads[4];
+    volatile int8_t note = 0; /* Flag used for determining if the beam is active */
+                              /* The program seems to work better when this is signed instead of unsigned */
+    volatile int16_t freq = -1; /* The value read from the ADC which determines which note to play */
+                                /* It is set to -1 when the beam is inactive */
+    int16_t freqReads[4]; /* Holds the values of four consecutive reads from the ADC.
+                          /* These values are then averaged and stored in FREQ */
 };
 
 #endif
