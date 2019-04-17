cbc8262579ee62e9e77ece1c6fad9836a72e2813
josh <hammerhead810@gmail.com>
2019-04-17 15:33:37 -0400

    Initialized freqReads to zero.

--- beam.h
+++ beam.h
@@ -16,7 +16,7 @@ Beam
                               /* The program seems to work better when this is signed instead of unsigned */
     volatile int16_t freq = -1; /* The value read from the ADC which determines which note to play */
                                 /* It is set to -1 when the beam is inactive */
-    int16_t freqReads[4]; /* Holds the values of four consecutive reads from the ADC.
+    int16_t freqReads[4] = {0}; /* Holds the values of four consecutive reads from the ADC.
                           /* These values are then averaged and stored in FREQ */
 
     volatile int8_t difference = -1; /* Used as a workaround to create atomic
